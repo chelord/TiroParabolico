58ad71b8d5a88e7d2d5078254f9dce7f0589a4bf
Francisco Canseco <pako_knc13@hotmail.com>
2014-02-24 14:56:16 -0600

    cambie mmdmdmdmdmdmdmdmmdb

--- src/tiroparabolico/TiroParabolico.java
+++ src/tiroparabolico/TiroParabolico.java
@@ -17,7 +17,7 @@ public class TiroParabolico {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int mdmdmdmdmdmdm;
+        int mdmdmdmdmdmdmh;
     }
     
 }
